69cf9d48f016b7a01ccb0cb1ba5054bbe06b44a4
Luciano Dias <luciano.dias@softdesign-rs.com.br>
2019-04-12 18:18:43 -0300

    adicionado upload de arquivo

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -3,7 +3,8 @@ const Box = require('../models/Box');
 
 class BoxController {
   async store(req, res) {
-    const box = await Box.create({ title: 'Teste' });
+    const { title } = req.body;
+    const box = await Box.create({ title });
     return res.json(box);
   }
 }
